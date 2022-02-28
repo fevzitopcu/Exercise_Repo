c932d752426be07062405c0e7e2720f288b37908
fevzitopcu <fevzitopcu@gmail.com>
2022-02-28 03:18:59 +0300

    phoneBook app

--- python/python-hands-on/cc-005-create-phonebook/cc-005-create-phonebook.py
+++ python/python-hands-on/cc-005-create-phonebook/cc-005-create-phonebook.py
@@ -1,45 +1,55 @@
 
-from cgitb import reset
-
-
-message = """
+def print_menu():
+    message = """
     Welcome to the phonebook application
     
     1. Find phone number
     2. Insert a phone number
     3. Delete a person from the phonebook
     4. Terminate
-
-    Select operation on Phonebook App (1/2/3) :
     """
+    return print(message)
 
 
-book = []
+phoneBook = {}
+print_menu()
 
 
-def phoneBook(selection):
-    
-    return True
+def find(phoneBook):
+    print("Find the phone number of : ")
+    name = input("")
+    if name in phoneBook:
+        print("The number is", phoneBook[name])
+    else:
+        print(f"{name}'s phone number couldn't find")
+
+def add(phoneBook):
+    print("Insert Name of the person")
+    name = input("Name: ")
+    print(f"Insert phone number of {name}")
+    phone = input("Number: ")
+    phoneBook[name] = phone
+    print(f"Phone number of {name} is inserted into the phonebook")
 
 
-def insertNumber(selection):
-
-    return True
-
-
-def deletePerson(selection):
-
-    return True
-
+def delete(phoneBook):
+    print("Whom to delete from phonebook :")
+    name = input("")
+    if name in phoneBook:
+        del phoneBook[name]
+        print(f"{name} is deleted from the phonebook")
+    else:
+        print(f"{name}'s phone number couldn't find")
 
 while True:
-    selection = input(message)
-    if selection == '1':
-        phoneBook(selection)
-    elif selection == '2':
-        insertNumber(selection)
-    elif selection == '3':
-        deletePerson(selection)
-    elif selection == '4':
-        print('Good By, closing the book...')
+    selection = (input("Select operation on Phonebook(1-4): "))
+
+    if selection == "1":
+        find(phoneBook)
+    elif selection == "2":
+        add(phoneBook)
+    elif selection == "3":
+        delete(phoneBook)
+    elif selection == "4":
+        print("Exiting Phonebook")
         break
